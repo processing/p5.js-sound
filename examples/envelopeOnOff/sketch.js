96c14bb97439bb21b58e5456bfdc9852fbdc0930
Jason Sigal <jason.sigal@gmail.com>
2014-08-02 17:01:48 -0400

    consolidating envelope examples

--- examples/envelopeOnOff/sketch.js
+++ examples/envelopeOnOff/sketch.js
@@ -4,10 +4,10 @@ var env;
 // Times and levels for the ADSR envelope
 var attackTime = 0.1;
 var attackLevel = .7;
-var decayTime = .1;
+var decayTime = .3;
 var sustainTime = 0.1;
-var sustainLevel = 0.1;
-var releaseTime = .1;
+var sustainLevel = 0.2;
+var releaseTime = .5;
 var duration = 1000;
 // Set the note trigger
 var trigger;
@@ -15,13 +15,14 @@ var trigger;
 // An index to count up the notes
 var note = 0;
 
+
 function setup(){
   createCanvas(600, 600);
   background(255);
 
   trigger = millis();
 
-  triOsc = new SawOsc();
+  triOsc = new TriOsc();
   triOsc.amp(0);
   triOsc.start();
 
@@ -33,24 +34,14 @@ function draw(){
   var size = 10;
   background(255, 255,255,20);
   ellipse( map ( (trigger - millis()) % duration, 1000, 0, 0, width) % width, map ( triOsc.getAmp(), 0, 1, height-size, 0), size, size);
-  var f = map (mouseX, 0, width, 40, 1000);
-  triOsc.freq(f);
-  console.log(mouseX);
 }
 
 function mousePressed(){
-
     // The envelope gets triggered with the oscillator as input and the times and levels we defined earlier
     env.triggerAttack(triOsc);
-
-    // Create the new trigger according to predefined durations and speed it up by deviding by 1.5
     trigger = millis() + duration;
 }
 
 function mouseReleased(){
-    // The envelope gets triggered with the oscillator as input and the times and levels we defined earlier
     env.triggerRelease(triOsc);
-
-    // Create the new trigger according to predefined durations and speed it up by deviding by 1.5
-    // trigger = millis() + duration;
 }
