ed86a462323e94db0f383e58f811d8d5251d75b5
Jason Sigal <jason.sigal@gmail.com>
2014-08-15 01:44:05 -0400

    tests

---
+++ test/tests/p5.Amplitude.js
@@ -0,0 +1,72 @@
+define(['chai'],
+  function(chai) {
+
+  var expect = chai.expect;
+
+  describe('p5.Amplitude', function() {
+    this.timeout(1000);
+
+    var sf, amp;
+
+    it('can be created', function(){
+      amp = new p5.Amplitude();
+    });
+
+    after(function(done){
+      // amp.getLevel();
+      // var ac = p5.prototype.getAudioContext();
+      // expect( amp.getLevel() ).to.not.equal(0.0);
+      expect( amp.getLevel() ).to.not.equal(1.0);
+      osc.dispose();
+      sf.dispose();
+      done();
+    });
+
+    var osc, oAmp;
+    it('accepts oscillator input', function() {
+      osc = new p5.Oscillator('square');
+      osc.amp(1);
+      osc.start();
+      osc.disconnect();
+      oAmp = new p5.Amplitude();
+      oAmp.setInput(osc);
+    });
+
+    it('gets oscillator level', function() {
+      setTimeout(function() {
+        console.log( 'unnormalized: ' + oAmp.getLevel() );
+        expect( oAmp.getLevel() ).to.be.closeTo(0.55, 0.25);
+      }, 100);
+    });
+
+    it('gets normalized osc level', function() {
+      setTimeout(function(cleanup) {
+        oAmp.toggleNormalize(true);
+        console.log( 'normalized: ' + oAmp.getLevel() );
+        expect( oAmp.getLevel() ).to.be.closeTo(1.0, 0.4);
+      }, 200);
+    });
+
+
+    it('loop a SoundFile with params, disconnected from master, setInput()', function(done){
+      p5.prototype.soundFormats('ogg', 'mp3');
+      sf = p5.prototype.loadSound('./testAudio/drum', function(){
+        sf.disconnect();
+        sf.loop(1,1,0.0, 0.05);
+        sf.connect(amp);
+        setTimeout(function() {
+          done();
+        }, 100);
+      });
+    });
+
+    it('stop getting level', function(done) {
+      sf.stop();
+      setTimeout(function() {
+        console.log( amp.getLevel() );
+        done();
+      }, 10);
+    });
+
+  });
+});
---
+++ test/tests/p5.Oscillator.js
@@ -0,0 +1,79 @@
+define(['chai'],
+  function(chai) {
+
+  var expect = chai.expect;
+
+  describe('p5.Oscillator', function() {
+    this.timeout(1000);
+
+    var osc = new p5.Oscillator();
+    var amp = new p5.Amplitude();
+
+    after(function() {
+      osc.dispose();
+    });
+
+    it('can be created and disposed', function() {
+      var o = new p5.Oscillator();
+      o.dispose();
+    });
+
+    it('starts and stops', function(done){
+      expect(osc.started).to.equal(false);
+      osc.start();
+      expect(osc.started).to.equal(true);
+      setTimeout(function() {
+        osc.stop();
+        done();
+      }, 100);
+    });
+
+    it('can be scheduled to stop', function(done){
+      osc.stop();
+      expect(osc.started).to.equal(false);
+      osc.start();
+      expect(osc.started).to.equal(true);
+      osc.stop(0.05);
+      setTimeout(function() {
+        expect(osc.started).to.equal(false);
+        done();
+      }, 55);
+    });
+
+    it('wont start again before stopping', function(done) {
+      expect(osc.started).to.equal(false);
+      setTimeout(function() {
+        osc.amp(1);
+        osc.start();
+        osc.stop();
+        expect(osc.started).to.equal(false);
+        osc.start();
+        osc.start();
+        amp.setInput(osc);
+        amp.getLevel();
+        setTimeout(function() {
+          console.log(amp.volMax);
+          expect(osc.started).to.equal(true);
+          expect(amp.volMax).not.equal(0.0);
+          osc.stop();
+          expect(osc.started).to.equal(false);
+          done();
+        }, 5);
+      }, 1);
+    });
+
+    it('can start in the future', function(done) {
+      expect(osc.started).to.equal(false);
+      osc.start(0.05);
+      // expect( amp.getLevel() ).to.be.closeTo(0.0, 0.5);
+      setTimeout(function() {
+        expect(osc.started).to.equal(true);
+        // expect( amp.getLevel ).to.not.equal(0.0);
+        osc.stop();
+        expect(osc.started).to.equal(false);
+        done();
+      }, 55);
+    });
+
+  });
+});
