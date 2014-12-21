451de46913931b966cd840397d3a5ce2c65d81b6
Jason Sigal <jason.sigal@gmail.com>
2014-12-21 15:42:31 -0500

    a better stereo panner, separate from 3d panner

--- src/panner.js
+++ src/panner.js
@@ -3,28 +3,54 @@ define(function (require) {
   'use strict';
 
   var p5sound = require('master');
+  var ac = p5sound.audiocontext;
 
+  // Stereo panner
   p5.Panner = function(input, output) {
-    var panner = p5sound.audiocontext.createPanner();
-    panner.panningModel = 'equalpower';
-    panner.distanceModel = 'linear';
-    panner.setPosition(0,0,0);
-    input.connect(panner);
-    panner.connect(output);
+    this.left = ac.createGain();
+    this.right = ac.createGain();
+    this.left.channelInterpretation = "discrete";
+    this.right.channelInterpretation = "discrete";
 
-    panner.pan = function(pval) {
-      pval = pval * 90.0;
-      var xDeg = parseInt(pval);
-      var zDeg = xDeg + 90;
-      if (zDeg > 90) {
-        zDeg = 180 - zDeg;
-      }
-      var x = Math.sin(xDeg * (Math.PI / 180));
-      var z = Math.sin(zDeg * (Math.PI / 180));
-      panner.setPosition(x, 0, z);
+    var splitter = ac.createChannelSplitter(2);
+
+    input.connect(splitter);
+
+    splitter.connect(this.left, 1);
+    splitter.connect(this.right, 0);
+
+    var mixer = ac.createChannelMerger(2);
+    this.left.connect(mixer, 0, 1);
+    this.right.connect(mixer, 0, 0);
+    mixer.connect(output);
+    console.log(output);
+  }
+
+  // -1 is left, +1 is right
+  p5.Panner.prototype.pan = function(val, t) {
+    var t = ac.currentTime || ac.currentTime + t;
+    var v = (val + 1) / 2;
+    var leftVal = Math.cos(v*Math.PI/2);
+    var rightVal = Math.sin(v * Math.PI/2);
+    this.left.gain.linearRampToValueAtTime(leftVal, t);
+    this.right.gain.linearRampToValueAtTime(rightVal, t);
+  }
+
+
+  // 3D panner
+  p5.Panner3D = function(input, output) {
+    var panner3D = ac.createPanner();
+    panner3D.panningModel = 'HRTF';
+    panner3D.distanceModel = 'linear';
+    panner3D.setPosition(0,0,0);
+    input.connect(panner3D);
+    panner3D.connect(output);
+
+    panner3D.pan = function(xVal, yVal, zVal) {
+      panner3D.setPosition(xVal, yVal, zVal);
     }
 
-    return panner;
+    return panner3D;
   };
 
 });
