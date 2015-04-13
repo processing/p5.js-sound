f88f0378a2ec80c0bd1411d9a9ba6cab1b4b6d47
b2renger <berenger.recoules@gmail.com>
2015-04-13 17:04:35 +0200

    update to granular sampler
    fix granuar sampler example, no more clicks.

--- examples/granular_sampler/sketch.js
+++ examples/granular_sampler/sketch.js
@@ -1,18 +1,9 @@
-/**
- *  Example: Granular Sampler
- *
- *  Granular synthesis is based on sampling at a small scale. You extract a small
- *  piece of sound (eg a 'grain') of variable length that you can playback at variable 
- *  speed. Grains can be layered on top of each other creating rich polyphonic textures.
- *  
- *  The mouse position drives the playback position along the X axis, and the playback
- *  rate along the Y axis.
- *
- *  The UP_ARROW and the DOWN_ARROW modify the grain length.
- *
- */
+// mouseX = playback position
+// mouseY = playback rate
+// up arrow  increase grain duration
+// down arrow decrease grain duration
 
-var source; // sound file
+var source_file; // sound file
 
 var src_length; // hold its duration
 
@@ -20,60 +11,57 @@ var peaks; // an array of peaks for the visual
 var pg;
 
 var voices = []; // an array of voices
-var num_voices = 40; 
-var poly_counter = 0; // keep track of the last active voice
-var grainDur = 1;
+var num_voices = 50; 
+var poly_counter = 0;
+
+var grainDur = 1; // length of the grain
 
 function preload(){
-    source = loadSound('../_files/Damscray_DancingTiger.mp3'); // preload the sound
+    source_file = loadSound('../_files/Soni_Ventorum_Wind_Quintet_-_08_-_Danzi_Wind_Quintet_Op_67_No_3_In_E-Flat_Major_4_Allegretto.mp3'); // preload the sound
 }
 
 function setup() {
-  
-  createCanvas(710, 400);
-  frameRate(20);
+  createCanvas(800, 250);
 
-  src_length = source.duration(); // store the sound duration
-  peaks = source.getPeaks(windowWidth); // get an array of peaks
+  src_length = source_file.duration(); // store the sound duration
+  peaks = source_file.getPeaks(); // get an array of peaks
 
   // draw the waveform to an off-screen graphic
-  pg = createGraphics(windowWidth,windowHeight);
+  pg = createGraphics(width,height);
   pg.background(180);
   pg.translate(0,height/2);
   pg.noFill();
   pg.stroke(0);
   for (var i = 0 ; i < peaks.length ; i++){
-        var x = map(i,0,peaks.length,0,windowWidth);
-        var y = map(peaks[i],0,1,0,windowHeight);
+        var x = map(i,0,peaks.length,0,width);
+        var y = map(peaks[i],0,1,0,height);
           pg.line(x,0,x,y);
           pg.line(x,0,x,-y);
     }
-    console.log("nb of peaks : "+peaks.length);
 
-  // build an array of granular voices 
- for (var i = 0 ; i < num_voices; i++){
-    var voice = new GranularVoice(source,grainDur); // duration half a second per voice
-    voices.push(voice); 
+   // build an array of granular voices 
+  for (var i = 0 ; i < num_voices; i++){
+     var voice = new GranularVoice(source_file,grainDur); // duration half a second per voice
+     voices.push(voice); 
 
-  }
-  console.log("source duration : " +src_length);
-  console.log("num voices : "+voices.length);
-
+   }
 }
 
 function draw() {
     background(180);
 
-    var start_play = map(mouseX,0,width,0, src_length   ); // map mouseX to position in the source
-    var pitch = map(mouseY,0,height,0.5,1.5); // map mouseY to the rate the sound will be played
-    // which voice to play
-    poly_counter += 1;
-    poly_counter = poly_counter%num_voices;
-    // play it
-    voices[poly_counter].playGrain(pitch,start_play,grainDur);
-
+    if (mouseIsPressed){
+      var start_play = map(mouseX,0,width,0,src_length); // map mouseX to position in the source
+      var pitch = map(mouseY,0,height,0.5,1.5); // map mouseY to the rate the sound will be played
+      // which voice to play
+      poly_counter += 1;
+      poly_counter = poly_counter%num_voices;
+      //play it
+      voices[poly_counter].playGrain(start_play,pitch);
+    }
+     
     image(pg,0,0); // display our waveform representation
-    // draw playhead
+    // draw playhead position 
     fill(255,255,180,150);
     noStroke();
     rect(mouseX,0,map(grainDur,0,src_length,0,width),height);
@@ -89,49 +77,73 @@ function keyPressed(){
     else if (keyCode === UP_ARROW){
         grainDur += 0.05;
     }
-    grainDur = constrain(grainDur,0.1,1.5);
+
+    grainDur = constrain(grainDur,0.1,25);
+    var newatt = grainDur*1/5;
+    var newrel = grainDur*1/5;
+
+    for (var i = 0 ; i < voices.length ; i++){
+      voices[i].setGrainDuration(grainDur);
+    }
 }
 
 
-// a granular voice class
 function GranularVoice(src,grLength){
+  // load a copy of our source in the main buffer
+  this.sound = src;
+  this.sound.playMode('sustain'); // we want polyphonic playing
+   
+  this.amp = 0.5;
 
-    this.sound = src;
-    this.sound.playMode('sustain');
-
-    this.amp = 0.05;
-
-    this.grainDur = grLength;
-
-    this.attack = this.grainDur*1/5;
-    this.decay = this.grainDur*1/5;
-    this.sustain = this.grainDur*2/5;
-    this.release = this.grainDur*1/5;
-
-    this.env = new p5.Env(this.attack,this.decay,this.sustain,this.release);
-
-    // apply the enveloppe
-    this.sound.amp(this.env);
-
+  // compute defaut parameters 
+  this.attack = 0.049;
+  this.release = 0.049;
+  this.grainDur = grLength - (this.attack + this.release);
 }
 
-GranularVoice.prototype.playGrain = function(rate,start,grLength){
-    this.sound.amp(this.env);
-    this.sound.play(0,rate,this.amp,start,grLength); 
-    this.env.play(this.sound); // play the enveloppe    
+GranularVoice.prototype.playGrain = function(start,rate){
+  var now = getAudioContext().currentTime; // get the time
+  
+  // play the grain
+  this.sound.play(0,rate,1,start,this.grainDur+1); // we need to play longer than grainDur because of the rate
+
+  // acess the gain node to control it with ramps
+  this.sound.source.gain.gain.cancelScheduledValues(now);
+  this.sound.source.gain.gain.setValueAtTime(0.0, now); // start at zero
+  this.sound.source.gain.gain.linearRampToValueAtTime(this.amp,now + this.attack); // go to amp during attack
+  this.sound.source.gain.gain.linearRampToValueAtTime(this.amp, now + (this.attack + this.grainDur) ); // stay during grain duration
+  this.sound.source.gain.gain.linearRampToValueAtTime(-0.0,now + (this.attack + this.grainDur  + this.release) ); // fo to zero for release  
 }
 
-GranularVoice.prototype.setGrainDur = function(newGrainDur){
-    this.grainDur = newGrainDur;
-    this.setAdsr(newGrainDur);
+GranularVoice.prototype.setAmp = function(newamp){
+  this.amp = newamp;
 }
 
-GranularVoice.prototype.setAdsr= function(newGrainDur){
-    this.attack = newGrainDur*1/5;
-    this.decay = newGrainDur*1/5;
-    this.sustain = newGrainDur*2/5;
-    this.release = newGrainDur*1/5;
 
-    this.env = new p5.Env(this.attack,this.decay,this.sustain,this.release);
-    this.sound.amp(this.env);
+GranularVoice.prototype.setAttack = function(newattack){
+  if(this.grainDur>(newattack+this.release)){
+    this.attack = newattack;
+  }
+  else {
+     throw 'new attack value out of range';
+  }
 }
+
+GranularVoice.prototype.setRelease = function(newrelease){
+  if(this.grainDur>(this.attack+newrelease)){
+    this.release = newrelease;
+  }
+  else {
+     throw 'new release value out of range';
+  }
+}
+  
+GranularVoice.prototype.setGrainDuration = function(newgraindur){
+  if(newgraindur > (this.attack+this.release)){
+     this.grainDur = newgraindur;
+  }
+  else {
+     throw 'new grain duration out of range';
+  }
+}
+  
