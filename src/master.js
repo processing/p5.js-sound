b043c82b65a6e5f65a82fe4749312094aa27444c
therewasaguy <jason.sigal@gmail.com>
2015-05-30 19:25:31 -0400

    v bump

--- src/master.js
+++ src/master.js
@@ -69,8 +69,8 @@ define(function (require) {
   };
 
 /**
-   *  Multiply the output volume (amplitude) of the sound of this sketch
-   *  between 0.0 (silence) and 1.0 (full volume).
+   *  <p>Scale the output of all sound in this sketch</p>
+   *  Scaled between 0.0 (silence) and 1.0 (full volume).
    *  1.0 is the maximum amplitude of a digital sound, so multiplying
    *  by greater than 1.0 may cause digital distortion. To
    *  fade, provide a <code>rampTime</code> parameter. For more
@@ -79,13 +79,15 @@ define(function (require) {
    *  Alternately, you can pass in a signal source such as an
    *  oscillator to modulate the amplitude with an audio signal.
    *
-   * <p><b>How This Works</b>: When you load the p5.sound module, it
+   *  <p><b>How This Works</b>: When you load the p5.sound module, it
    *  creates a single instance of p5sound. All sound objects in this
    *  module output to p5sound before reaching your computer's output.
    *  So if you change the amplitude of p5sound, it impacts all of the
    *  sound in this module.</p>
    *
-   *  @method  setMasterVolume
+   *  <p>If no value is provided, returns a Web Audio API Gain Node</p>
+   *
+   *  @method  masterVolume
    *  @param {Number|Object} volume  Volume (amplitude) between 0.0
    *                                     and 1.0 or modulating signal/oscillator
    *  @param {Number} [rampTime]  Fade for t seconds
@@ -109,8 +111,6 @@ define(function (require) {
       return p5sound.output.gain;
     }
   };
-
-
 
   /**
    *  p5.soundOut is the p5.sound master output. It sends output to
