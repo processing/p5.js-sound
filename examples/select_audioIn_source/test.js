7a6c2bfa8b6bffc11e62e281a43dba01e2acc2c0
Seth Kranzler <sethkranzler@gmail.com>
2015-06-25 13:32:05 -0400

    finished example

--- examples/select_audioIn_source/test.js
+++ examples/select_audioIn_source/test.js
@@ -1,47 +1,56 @@
-var audioGrab, audioSources;
-
+var audioGrab;
+var numSources = 0;
 var fft = [];
 var audioGrabArray = [];
 
 function setup() {
     createCanvas(512, 400);
-
+    textSize(32);
+    textAlign(LEFT, CENTER);
+    //we will use a new p5AudioIn to enumerate the
+    //audio devices. This won't connect to any output.
     audioGrab = new p5.AudioIn();
     audioGrab.getSources(function(sourceList) {
-        //set the source to the first item in the inputSources array
-        audioGrab.setSource(0);
-        audioGrab.start();
-        console.log(sourceList);
-        audioSources = sourceList;
 
-        for (var i = 0; i < sourceList.length; i++) {
+        numSources = sourceList.length;
+        //creating an array of all the available sources
+        for (var i = 0; i < numSources; i++) {
             audioGrabArray[i] = new p5.AudioIn();
             audioGrabArray[i].setSource(i);
             audioGrabArray[i].start();
 
+            //from the FFT example
             fft[i] = new p5.FFT();
             fft[i].setInput(audioGrabArray[i]);
-            //this will print the array of sources
         }
     });
 }
 
 function draw() {
     background(200);
-
-    numSources = audioSources.length;
     for (var i = 0; i < numSources; i++) {
+        var yPos = ((i + 1) / numSources) * height;
         var spectrum = fft[i].analyze();
+
         stroke();
-        line(0, ((i + 1) / numSources) * height, width, ((i + 1) / numSources) * height);
+        line(0, ((i + 1) / numSources) * height, width, yPos);
         beginShape();
         vertex(0, ((i + 1) / numSources) * height);
 
         noStroke();
         fill(0, 255, 255);
         for (j = 0; j < spectrum.length; j++) {
-            vertex(j, map(spectrum[j], 0, 1023, ((i + 1) / numSources) * height, 0));
+            vertex(j, map(spectrum[j], 0, 1023, yPos, 0));
         }
         endShape();
+
+        //without using HTTPS, Chrome cannot give us device names (such as
+        //'mic' or 'soundflower'). We can use this visualization to determine
+        //which input array position number is the one we want.
+        //The first two are typically "default" and "mic" 
+        //(default usually is the mic)
+
+        fill(0);
+        text("input array position: " + i, 10, yPos - 0.5 * height / numSources);
     }
 }
