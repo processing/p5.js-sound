81bea671e8946792d944d22880d38893791a2dc7
therewasaguy <jason.sigal@gmail.com>
2015-10-18 16:16:47 -0400

    ios tweaks

--- src/sndcore.js
+++ src/sndcore.js
@@ -62,12 +62,151 @@
 define(function (require) {
   'use strict';
 
-  /**
-   * Web Audio SHIMS and helper functions to ensure compatability across browsers
-   */
+  /* AudioContext Monkeypatch
+     Copyright 2013 Chris Wilson
+     Licensed under the Apache License, Version 2.0 (the "License");
+     you may not use this file except in compliance with the License.
+     You may obtain a copy of the License at
+         http://www.apache.org/licenses/LICENSE-2.0
+     Unless required by applicable law or agreed to in writing, software
+     distributed under the License is distributed on an "AS IS" BASIS,
+     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+     See the License for the specific language governing permissions and
+     limitations under the License.
+  */
+  (function (global, exports, perf) {
+    'use strict';
 
-  // If window.AudioContext is unimplemented, it will alias to window.webkitAudioContext.
-  window.AudioContext = window.AudioContext || window.webkitAudioContext;
+    function fixSetTarget(param) {
+      if (!param) // if NYI, just return
+        return;
+      if (!param.setTargetAtTime)
+        param.setTargetAtTime = param.setTargetValueAtTime;
+    }
+
+    if (window.hasOwnProperty('webkitAudioContext') &&
+        !window.hasOwnProperty('AudioContext')) {
+      window.AudioContext = webkitAudioContext;
+
+      if (!AudioContext.prototype.hasOwnProperty('createGain'))
+        AudioContext.prototype.createGain = AudioContext.prototype.createGainNode;
+      if (!AudioContext.prototype.hasOwnProperty('createDelay'))
+        AudioContext.prototype.createDelay = AudioContext.prototype.createDelayNode;
+      if (!AudioContext.prototype.hasOwnProperty('createScriptProcessor'))
+        AudioContext.prototype.createScriptProcessor = AudioContext.prototype.createJavaScriptNode;
+      if (!AudioContext.prototype.hasOwnProperty('createPeriodicWave'))
+        AudioContext.prototype.createPeriodicWave = AudioContext.prototype.createWaveTable;
+
+
+      AudioContext.prototype.internal_createGain = AudioContext.prototype.createGain;
+      AudioContext.prototype.createGain = function() {
+        var node = this.internal_createGain();
+        fixSetTarget(node.gain);
+        return node;
+      };
+
+      AudioContext.prototype.internal_createDelay = AudioContext.prototype.createDelay;
+      AudioContext.prototype.createDelay = function(maxDelayTime) {
+        var node = maxDelayTime ? this.internal_createDelay(maxDelayTime) : this.internal_createDelay();
+        fixSetTarget(node.delayTime);
+        return node;
+      };
+
+      AudioContext.prototype.internal_createBufferSource = AudioContext.prototype.createBufferSource;
+      AudioContext.prototype.createBufferSource = function() {
+        var node = this.internal_createBufferSource();
+        if (!node.start) {
+          node.start = function ( when, offset, duration ) {
+            if ( offset || duration )
+              this.noteGrainOn( when || 0, offset, duration );
+            else
+              this.noteOn( when || 0 );
+          };
+        } else {
+          node.internal_start = node.start;
+          node.start = function( when, offset, duration ) {
+            if( typeof duration !== 'undefined' )
+              node.internal_start( when || 0, offset, duration );
+            else
+              node.internal_start( when || 0, offset || 0 );
+          };
+        }
+        if (!node.stop) {
+          node.stop = function ( when ) {
+            this.noteOff( when || 0 );
+          };
+        } else {
+          node.internal_stop = node.stop;
+          node.stop = function( when ) {
+            node.internal_stop( when || 0 );
+          };
+        }
+        fixSetTarget(node.playbackRate);
+        return node;
+      };
+
+      AudioContext.prototype.internal_createDynamicsCompressor = AudioContext.prototype.createDynamicsCompressor;
+      AudioContext.prototype.createDynamicsCompressor = function() {
+        var node = this.internal_createDynamicsCompressor();
+        fixSetTarget(node.threshold);
+        fixSetTarget(node.knee);
+        fixSetTarget(node.ratio);
+        fixSetTarget(node.reduction);
+        fixSetTarget(node.attack);
+        fixSetTarget(node.release);
+        return node;
+      };
+
+      AudioContext.prototype.internal_createBiquadFilter = AudioContext.prototype.createBiquadFilter;
+      AudioContext.prototype.createBiquadFilter = function() {
+        var node = this.internal_createBiquadFilter();
+        fixSetTarget(node.frequency);
+        fixSetTarget(node.detune);
+        fixSetTarget(node.Q);
+        fixSetTarget(node.gain);
+        return node;
+      };
+
+      if (AudioContext.prototype.hasOwnProperty( 'createOscillator' )) {
+        AudioContext.prototype.internal_createOscillator = AudioContext.prototype.createOscillator;
+        AudioContext.prototype.createOscillator = function() {
+          var node = this.internal_createOscillator();
+          if (!node.start) {
+            node.start = function ( when ) {
+              this.noteOn( when || 0 );
+            };
+          } else {
+            node.internal_start = node.start;
+            node.start = function ( when ) {
+              node.internal_start( when || 0);
+            };
+          }
+          if (!node.stop) {
+            node.stop = function ( when ) {
+              this.noteOff( when || 0 );
+            };
+          } else {
+            node.internal_stop = node.stop;
+            node.stop = function( when ) {
+              node.internal_stop( when || 0 );
+            };
+          }
+          if (!node.setPeriodicWave)
+            node.setPeriodicWave = node.setWaveTable;
+          fixSetTarget(node.frequency);
+          fixSetTarget(node.detune);
+          return node;
+        };
+      }
+    }
+
+    if (window.hasOwnProperty('webkitOfflineAudioContext') &&
+        !window.hasOwnProperty('OfflineAudioContext')) {
+      window.OfflineAudioContext = webkitOfflineAudioContext;
+    }
+
+  }(window));
+  // <-- end MonkeyPatch.
 
   // Create the Audio Context
   var audiocontext = new window.AudioContext();
@@ -84,30 +223,6 @@ define(function (require) {
   p5.prototype.getAudioContext = function(){
     return audiocontext;
   };
-
-  // Polyfills & SHIMS (inspired by tone.js and the AudioContext MonkeyPatch https://github.com/cwilso/AudioContext-MonkeyPatch/ (c) 2013 Chris Wilson, Licensed under the Apache License) //
-
-  if (typeof audiocontext.createGain !== 'function'){
-    audiocontext.createGain = audiocontext.createGainNode;
-  }
-  if (typeof audiocontext.createDelay !== 'function'){
-    audiocontext.createDelay = audiocontext.createDelayNode;
-  }
-  if (typeof window.AudioBufferSourceNode.prototype.start !== 'function'){
-    window.AudioBufferSourceNode.prototype.start = window.AudioBufferSourceNode.prototype.noteGrainOn;
-  }
-  if (typeof window.AudioBufferSourceNode.prototype.stop !== 'function'){
-    window.AudioBufferSourceNode.prototype.stop = window.AudioBufferSourceNode.prototype.noteOff;
-  }
-  if (typeof window.OscillatorNode.prototype.start !== 'function'){
-    window.OscillatorNode.prototype.start = window.OscillatorNode.prototype.noteOn;
-  }
-  if (typeof window.OscillatorNode.prototype.stop !== 'function'){
-    window.OscillatorNode.prototype.stop = window.OscillatorNode.prototype.noteOff; 
-  }
-  if (!window.AudioContext.prototype.hasOwnProperty('createScriptProcessor')){
-    window.AudioContext.prototype.createScriptProcessor = window.AudioContext.prototype.createJavaScriptNode;
-  }
 
   // Polyfill for AudioIn, also handled by p5.dom createCapture
   navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia;
@@ -158,22 +273,28 @@ define(function (require) {
   // if it is iOS, we have to have a user interaction to start Web Audio
   // http://paulbakaus.com/tutorials/html5/web-audio-on-ios/
   var iOS = ( navigator.userAgent.match(/(iPad|iPhone|iPod)/g) ? true : false );
-
   if (iOS) {
-    window.addEventListener('touchend', function() {
+    var iosStarted = false;
+    var startIOS = function() {
+      if (iosStarted) return;
 
-    // create empty buffer
-    var buffer = audiocontext.createBuffer(1, 1, 22050);
-    var source = audiocontext.createBufferSource();
-    source.buffer = buffer;
+      // create empty buffer
+      var buffer = audiocontext.createBuffer(1, 1, 22050);
+      var source = audiocontext.createBufferSource();
+      source.buffer = buffer;
 
-    // connect to output (your speakers)
-    source.connect(audiocontext.destination);
+      // connect to output (your speakers)
+      source.connect(audiocontext.destination);
+      // play the file
+      source.start(0);
+      console.log('start ios!');
 
-    // play the file
-    source.start(0);
-
-    }, false);
+      if (audiocontext.state === 'running') {
+        iosStarted = true;
+      }
+    };
+    document.addEventListener('touchend', startIOS, false);
+    document.addEventListener('touchstart', startIOS, false);
 
     // TO DO: fake touch event so that audio will just start
   }
