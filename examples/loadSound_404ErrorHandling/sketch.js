2119f111d7c69d7e770533b41a1fb1436be7ca00
therewasaguy <jason.sigal@gmail.com>
2015-11-24 09:14:03 -0800

    uncomment logging in example

--- examples/loadSound_404ErrorHandling/sketch.js
+++ examples/loadSound_404ErrorHandling/sketch.js
@@ -13,11 +13,11 @@ function soundReady(soundFile){
   soundFile.play();
 }
 
+// the error has the following properties:
 function soundError(e) {
-	console.log(e);
-	// console.log('New error:');
-	// console.log('- name: ' + e.name);
-	// console.log('- message: ' + e.message);
-	// console.log('- stack: ' + e.stack);
-	// console.log('- failed path: ' + e.failedPath);
+	console.log('New error:');
+	console.log('- name: ' + e.name);
+	console.log('- message: ' + e.message);
+	console.log('- stack: ' + e.stack);
+	console.log('- failed path: ' + e.failedPath);
 }
