edc7f4131ddf3e8b849f0c73b902ab4d674a054e
Jeevan <jeevanfarias@gmail.com>
2017-06-11 12:16:09 -0400

    rough structure of compressor

--- src/compressor.js
+++ src/compressor.js
@@ -6,6 +6,21 @@ define(function (require) {
 
 	p5.Compressor = function() {
 		Effect.call(this);
+
+		this.compressor = this.ac.createDynamicsCompressorNode();
+
 	};
 	p5.Compressor.prototype = Object.create(Effect.prototype);
+
+	p5.Compressor.prototype.process = function(src,threshold,wetdry) {
+		src.connect(this.input);
+		this.set(threshold,wetdry);
+	};
+
+	p5.Compressor.prototype.dispose = function() {
+		Effec.prototype.dispose.apply(this);
+
+		this.compressor.disconnect();
+		this.compressor = undefined;
+	}
 });
