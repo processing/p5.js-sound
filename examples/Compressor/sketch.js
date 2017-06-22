97ffadc965d773de59bbc3e4646cb6af4aadb9e7
Jeevan <jeevanfarias@gmail.com>
2017-06-22 10:54:13 +0200

    setup objects for compressor example

--- examples/Compressor/sketch.js
+++ examples/Compressor/sketch.js
@@ -1,19 +1,29 @@
-/**
- *  Example: Apply a p5.Compressor filter to a p5.SoundFile.
- *  Visualize the sound with FFT.
- *  Map mouseX to the the filter's cutoff frequency
- *  and mouseY to resonance/width of the a BandPass filter
- */
+var fftHeight;
+var fftWidth;
 
 var soundFile;
 var fft;
 
-var description = 'loading';
-var p;
 
-var attack, threshold;
+
 var compressor;
-var ui = [];
+
+//UI Objects
+var knobs = [];
+var threshCntrl;
+
+
+//knob info
+
+//colors
+var knobBckg;
+var knobLine;
+var threshLineCol;
+
+//dimensions
+var knobRad;
+var knobLineLen;
+
 
 function preload() {
   soundFormats('mp3', 'ogg');
@@ -21,9 +31,12 @@ function preload() {
 }
 
 function setup() {
-  createCanvas(710, 512);
-  fill(255, 40, 255);
 
+  createCanvas(710, 256);
+
+
+  fftHeight = 0.75*height;
+  fftWidth = 0.75*width;
   // loop the sound file
   // soundFile.loop();
 
@@ -38,12 +51,39 @@ function setup() {
   fft = new p5.FFT();
 
 
-  for (var i = 0; i < 5; i++) {
-    ui[i] = new Slider(i);
+  soundFile.loop();
+
+
+  //Create Knobs
+  var x = 0.0625*width;
+  var y = .25*height;
+  knobs[0] = new Knob('ratio');
+  knobs[1] = new Knob('attack');
+  knobs[2] = new Knob('release');
+  for (var i = 0; i < knobs.length; i++) {
+    knobs[i].x = x;
+    knobs[i].y = y + y*i;
+    console.log(knobs[i]);
   }
 
-  p = createP(description);
-  var p2 = createP('Draw the array returned by FFT.analyze( ). This represents the frequency spectrum, from lowest to highest frequencies.');
+  knobs[3] = new Knob('wetdry');
+  knobs[3].x = width-x;
+  knobs[3].y = 3*y;
+  console.log(knobs[3]);
+  knobRad = .15*height;
+  knobLineLen = knobRad/2;
+
+
+  //create Threshold control
+  threshCntrl = new ThreshLine();
+
+  knobBckg = color(150);
+  knobLine = color(30);
+
+
+
+  threshLineCol = color(30);
+
 
 }
 //attack knee ratio threshold release
@@ -51,93 +91,92 @@ function setup() {
 function draw() {
   background(255);
  
+  
 
 
-  // Map mouseX to a the cutoff frequency for our lowpass filter
-  //attack = map (mouseX, 0, width, 0, 1);
-  // // Map mouseY to resonance/width
-  //threshold = map(mouseY, 0, height, -100, 0);
-  // // set filter parameters
-  //compressor.set(attack, 30, 12, threshold, 0.25);
-
-  // Draw every value in the FFT spectrum analysis where
-  // x = lowest (10Hz) to highest (22050Hz) frequencies,
-  // h = energy / amplitude at that frequency
+  fill(180);
+ //sound wave
   var spectrum = fft.analyze();
   noStroke();
-  for (var i = 0; i< spectrum.length; i++){
-    var x = map(i, 0, spectrum.length, 0, width);
-    var h = -height/2 + map(spectrum[i], 0, 255, height/2, 0);
-    rect(x, height/2, width/spectrum.length, h) ;
-  }
+ for (var i = 0; i< spectrum.length; i++){
+   var x = map(i, 0, spectrum.length, 0.125*width, fftWidth);
+   var h = -fftHeight + map(spectrum[i], 0, 255, fftHeight, 0.125*height);
+   rect(x, fftHeight, fftWidth/spectrum.length, h) ;
+ }
 
-  for (var i = 0; i < 5; i++) {
-    ui[i].move()
-    ui[i].display();
-  }
 
-  updateDescription();
+
+ for (var i = 0; i < knobs.length; i++) {
+   knobs[i].display();
+ }
+ threshCntrl.display();
+
 }
 
-function Slider(i){
-  this.bckg = color(30);
-  this.sCol = color(255, 40, 255);
-  this.x = 137.5*i;
-  this.y = 300;
-  this.h = 100
-  this.cntrl = 300;
-  this.ind = i
+function ThreshLine() {
+  this.x = 0.125*width;
+  this.y = 0.15*height;
+
+  this.length = fftWidth;
 
   this.display = function () {
-    fill(this.bckg);
-    rect(this.x, this.y, 20, this.h);
+    stroke(threshLineCol);
+    line(this.x,this.y, this.length, this.y)
+  };
 
-    fill(this.sCol);
-    rect(this.x, this.cntrl, 20,20);
-  }
-
-  this.move = function(){
-    if (onSlider(this.x,this.cntrl)){
-      this.cntrl = mouseY;
-      if (this.cntrl > this.y + this.h-20 ){this.cntrl = this.h + this.y -20;}
-      if (this.cntrl < this.y) { this.cntrl = this.y}
-
-    }
-    
-  }
+  this.move = function (newY) {
+    this.y = newY;
+  };
 
 }
 
-// function mouseClicked(){
-  
-//   if (onSlider()){
+function Knob(type){
+  this.type = type;
+   this.range = getRange(type);
+   this.default = getDefault(type);
+   this.current = this.default;
+   this.curAngle = getAngle(this.range, this.current);
+    this.x;
+    this.y;
 
-//     ui[onSlider()].move();
-//   }
+  this.display = function () {
+    noStroke();
+    fill(knobBckg);
+    ellipse(this.x, this.y, knobRad,knobRad);
 
-// }
+    //draw the indicator line from knob center
+    translate(this.x,this.y);
+    rotate(this.curAngle);
+    stroke(knobLine);
+    line(0,0,0,knobLineLen);
+    rotate(-this.curAngle);
+    translate(-this.x,-this.y);
 
-function onSlider(x,y){
-  //if (mouseIsPressed) {console.log("hello");}
-      // console.log("mouseX " + mouseX);
-      // console.log("x" + x +", x+20 " + (x+20));
-
-      // console.log("mouseY " + mouseY);
-      // console.log("y" + y +", y+20 " + (y+20));
-  if (mouseX > x && mouseX < x + 20 &&
-      mouseY > y && mouseY < y + 20 && mouseIsPressed){
-
-    return true;
-  } else{
-    return false;
+   
+    noStroke();
+    text(type, this.x - knobLineLen, this.y+knobLineLen, knobRad,knobRad);
+    // rectMode(CORNER);
   }
+
+  // this.turn = function () {
+  //   this.curAngle = get
+  // }
+
+
 }
 
 
-// Change description text if the song is loading, playing or paused
-function updateDescription() {
-    description = 'attack = ' + attack + ' knee = ' + compressor.compressor.knee.value + ' ratio= '
-        + compressor.compressor.ratio.value + ' threshold= ' + compressor.compressor.threshold.value +
-        ' release= '+ compressor.compressor.release.value;
-    p.html(description);
+function getRange(type){
+  return [0,1];
+}
+
+function getDefault(type){
+  return 0.5;
+}
+function getAngle(range,current){
+  return 90;
+}
+
+function mousePressed(){
+
 }
