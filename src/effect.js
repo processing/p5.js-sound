2232e806be4cf1d560f1dcbd514eb997fcf1c825
Jason Sigal <jason.sigal@gmail.com>
2017-07-28 02:16:38 -0400

    p5.Effect disposes wet dry nodes (#208)

--- src/effect.js
+++ src/effect.js
@@ -139,6 +139,12 @@ define(function (require) {
 		this.output.disconnect();
 		this.output = undefined;
 
+    this._drywet.disconnect();
+    delete this._drywet;
+
+    this.wet.disconnect();
+    delete this.wet;
+
 		this.ac = undefined;
 	};
 
