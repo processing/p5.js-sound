7e7a8818304dfb39c4a779e2085aace4c5ecb4b0
therewasaguy <jason.sigal@gmail.com>
2017-12-14 22:11:22 -0500

    fix AudioIn getSources #231 AudioIn uses window.navigator.mediaDevices getUserMedia and enumerateDevices
    add AudioIn test
    
    AudioIn uses audio context sample rate (fix a bug in Firefox)

---
+++ test/tests/p5.AudioIn.js
@@ -0,0 +1,40 @@
+define(['chai'],
+  function(chai) {
+
+  var expect = chai.expect;
+
+  describe('p5.AudioIn', function() {
+    it('can be created and disposed', function(){
+      var mic = new p5.AudioIn();
+      mic.dispose();
+    });
+
+    it('can be started and stopped', function() {
+      var mic = new p5.AudioIn();
+      mic.start(function() {
+        mic.stop();
+      });
+    });
+
+    it('can get sources', function(done) {
+      var mic = new p5.AudioIn();
+      mic.getSources(function(sources) {
+        console.log(sources);
+        expect(sources).to.be.an('array');
+        done();
+      });
+    });
+
+    it('can set source', function(done) {
+      var mic = new p5.AudioIn();
+      expect(mic.currentSource).to.be.null;
+
+      return mic.getSources(function(sources) {
+        mic.setSource(0);
+        expect(mic.currentSource).to.be(0);
+        done();
+      });
+    });
+  });
+
+});
