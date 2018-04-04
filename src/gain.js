c07f0f478ea1929db31a0df19066737f34797fea
piersh <piersh@hotmail.com>
2018-04-04 11:37:10 -0700

    fix reference assets path

--- src/gain.js
+++ src/gain.js
@@ -19,8 +19,8 @@ define(function (require) {
    *
    * function preload(){
    *   soundFormats('ogg', 'mp3');
-   *   sound1 = loadSound('../_files/Damscray_-_Dancing_Tiger_01');
-   *   sound2 = loadSound('../_files/beat.mp3');
+   *   sound1 = loadSound('assets/Damscray_-_Dancing_Tiger_01');
+   *   sound2 = loadSound('assets/beat.mp3');
    * }
    *
    * function setup() {
