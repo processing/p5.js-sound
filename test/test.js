19acb4b8d316c01776d5edead8828c375df0263d
therewasaguy <jason.sigal@gmail.com>
2018-05-23 01:46:34 -0400

    add some tests for synths and audiovoice

--- test/test.js
+++ test/test.js
@@ -6,10 +6,21 @@ require.config({
 	}
 });
 
-var allTests = ['tests/p5.SoundFile', 'tests/p5.Amplitude', 
-	'tests/p5.Oscillator', 'tests/p5.Distortion', 
-	'tests/p5.Effect','tests/p5.Filter', 'tests/p5.FFT', 'tests/p5.Compressor',
-	'tests/p5.EQ', 'tests/p5.AudioIn'];
+var allTests = [
+	'tests/p5.SoundFile',
+	'tests/p5.Amplitude',
+	'tests/p5.Oscillator',
+	'tests/p5.Distortion',
+	'tests/p5.Effect',
+	'tests/p5.Filter',
+	'tests/p5.FFT',
+	'tests/p5.Compressor',
+	'tests/p5.EQ',
+	'tests/p5.AudioIn',
+	'tests/p5.AudioVoice',
+	'tests/p5.MonoSynth',
+	'tests/p5.PolySynth'
+];
 
 p5.prototype.masterVolume(0);
 
---
+++ test/tests/p5.PolySynth.js
@@ -0,0 +1,37 @@
+define(['chai'],
+  function(chai) {
+
+  var expect = chai.expect;
+
+  describe('p5.PolySynth', function() {
+    var audioContext = p5.prototype.getAudioContext();
+
+    it('can be created and disposed', function() {
+      var polySynth = new p5.PolySynth();
+      polySynth.dispose();
+    });
+
+    it('keeps track of the number of voicesInUse', function() {
+      var polySynth = new p5.PolySynth();
+      var noteDuration = 0.01;
+
+      var noteTriggerTime = audioContext.currentTime;
+      var noteActiveTime = noteTriggerTime + noteDuration/2;
+      var noteDoneTime = noteTriggerTime + noteDuration;
+
+      expect(polySynth._voicesInUse.getValueAtTime(noteTriggerTime)).to.equal(0);
+
+      polySynth.play('A2', 0, 0, noteDuration);
+      expect(polySynth._voicesInUse.getValueAtTime(noteActiveTime)).to.equal(1);
+      polySynth.play('A3', 0, 0, noteDuration);
+      expect(polySynth._voicesInUse.getValueAtTime(noteActiveTime)).to.equal(2);
+      polySynth.play('A4', 0, 0, noteDuration);
+      expect(polySynth._voicesInUse.getValueAtTime(noteActiveTime)).to.equal(3);
+
+      expect(polySynth._voicesInUse.getValueAtTime(noteDoneTime)).to.equal(0);
+
+      polySynth.dispose();
+    });
+  });
+
+});
