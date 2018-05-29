afb36a6997344516af6386bf14d3b182c5d5f610
Jason Sigal <jsigal@spotify.com>
2018-05-29 11:15:04 +0200

    add startAudioContext method

--- src/audiocontext.js
+++ src/audiocontext.js
@@ -17,6 +17,39 @@ define(function () {
     return audiocontext;
   };
 
+  /**
+   * A user gesture is required to start or resume the AudioContext
+   * in some browsers. Place this method in an event handle for user
+   * interaction, such as touchStarted or mousePressed.
+   * @method  startAudioContext
+   * @return {Promise}
+   * @example
+   * <div><code>
+   * function draw() {
+   *   background(255);
+   *   textAlign(CENTER);
+   *   if (getAudioContext().state !== 'running') {
+   *     text('click to start audio', width/2, height/2);
+   *   } else {
+   *     text('audio is enabled', width/2, height/2);
+   *   }
+   * }
+   *
+   * function touchStarted() {
+   *   startAudioContext();
+   *   var synth = new p5.MonoSynth();
+   *   synth.play('A4', 0.5, 0, 0.2);
+   * }
+   * </div></code>
+   */
+  p5.prototype.startAudioContext = function() {
+    if (audiocontext.state === 'running') {
+      return Promise.resolve();
+    }
+    // will reject if audiocontet state was 'closed', otherwise success
+    return audiocontext.resume();
+  };
+
   // if it is iOS, we have to have a user interaction to start Web Audio
   // http://paulbakaus.com/tutorials/html5/web-audio-on-ios/
   var iOS =  navigator.userAgent.match(/(iPad|iPhone|iPod)/g) ? true : false ;
@@ -34,7 +67,6 @@ define(function () {
       source.connect(audiocontext.destination);
       // play the file
       source.start(0);
-      console.log('start ios!');
 
       if (audiocontext.state === 'running') {
         iosStarted = true;
