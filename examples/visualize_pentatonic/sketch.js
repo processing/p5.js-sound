8d03e69792953650cbf9d5cd1f74b05693288fde
junshern <chanjunshern@gmail.com>
2018-06-25 22:04:11 +0100

    Add particle system to visualize_pentatonic example

--- examples/visualize_pentatonic/sketch.js
+++ examples/visualize_pentatonic/sketch.js
@@ -1,59 +1,52 @@
 var sloop;
-
-var pentatonic = ['A','C','D','E','G'];
-var pitch_class_map = {'A':0,'C':1,'D':2,'E':3,'G':4};
+var pentatonic_scale = ['A','C','D','E','G'];
+var pitchClass_map = {'A':0,'C':1,'D':2,'E':3,'G':4};
 var numOctaves = 6;
-
-var currentNote = 'C3';
-var noteCount = 0;
-var prevNoteCount = 0;
+var baseOctave = 2;
+var heightLevel;
+var system;
 
 function setup() {
-  createCanvas(500,500);
-  background(0);
-  textAlign(CENTER, CENTER);
+  createCanvas(720, 400);
+  // Particles to visualize notes
+  system = new ParticleSystem(createVector(width/2, 50));
   // Create a synth to make sound with
   synth = new p5.PolySynth();
-  // Create SoundLoop with 16th-note-long loop interval
-  sloop = new p5.SoundLoop(soundLoop, "16n"); 
-  sloop.bpm = 60; // 60 beats per minute
+  // Create SoundLoop with 8th-note-long loop interval
+  sloop = new p5.SoundLoop(soundLoop, "8n"); 
+  sloop.bpm = 80; // 80 beats per minute
+}
+
+function draw() {
+  background(50);
+  // Get mouse height level
+  stroke(255, 100);
+  heightLevel = round(numOctaves * (height - mouseY) / height);
+  line(0, height - heightLevel*height/numOctaves, 
+    width, height - heightLevel*height/numOctaves);
+  // Update particle system
+  system.run();
+  // Play/pause controls
+  fill(255);
+  textAlign(CENTER, CENTER);
+  if (sloop.isPlaying) text('Click to Pause', width/2, height/2);
+  else text('Click to Play', width/2, height/2);
 }
 
 function soundLoop(cycleStartTime) {
   // Pick a random note, note octave based on mouse height
-  octave = round(numOctaves * (height - mouseY) / height);
-  currentNote = random(pentatonic) + str(octave);
+  var pitchClass = random(pentatonic_scale);
+  var octave = baseOctave + heightLevel;
+  var currentNote = pitchClass + str(octave);
   // Play sound
-  semiquaverSeconds = this._convertNotation('16n'); // 16th note = semiquaver duration
-  synth.play(currentNote, 0.8, cycleStartTime, semiquaverSeconds);
-
-  noteCount++;
-}
-
-function draw() {
-  background(0, 25); // 10% opacity gives a visual fade effect
-
-  fill(255);
-  if (sloop.isPlaying) {
-    text('Click to Pause', width/2, height/2);
-  } else {
-    text('Click to Play', width/2, height/2);
-  }
-
-  // Draw if there is a new note
-  if (noteCount != prevNoteCount) {
-    drawNote(currentNote);
-    prevNoteCount = noteCount;
-  }
-}
-
-function drawNote(noteName) {
-  pitch_class = noteName.slice(0,-1);
-  octave = Number(noteName.slice(-1));
-  pitch_class_pos = pitch_class_map[pitch_class];
-
-  fill(random(255), random(255), random(255));
-  ellipse(width/10 + pitch_class_pos*width/5, height - octave*height/numOctaves, 20, 20);
+  var velocity = 0.1; // Between 0-1
+  quaverSeconds = this._convertNotation('8n'); // 8th note = quaver duration
+  synth.play(currentNote, velocity, cycleStartTime, quaverSeconds);
+  // Add a particle to visualize the note
+  var pitchClassIndex = pentatonic_scale.indexOf(pitchClass);
+  var xpos = width / (pentatonic_scale.length * 2) + pitchClassIndex * width / pentatonic_scale.length;
+  var ypos = height - heightLevel * height / numOctaves;
+  system.addParticle(xpos, ypos);
 }
 
 function mouseClicked() {
@@ -63,3 +56,55 @@ function mouseClicked() {
     sloop.start();
   }
 }
+
+// A simple Particle class
+var Particle = function(position) {
+  this.acceleration = createVector(0, 0.1);
+  this.velocity = createVector(random(-1, 1), random(-3, -1));
+  this.position = position.copy();
+  this.lifespan = 255;
+  this.color = [random(255), random(255), random(255)];
+};
+
+Particle.prototype.run = function() {
+  this.update();
+  this.display();
+};
+
+// Method to update position
+Particle.prototype.update = function(){
+  this.velocity.add(this.acceleration);
+  this.position.add(this.velocity);
+  this.lifespan -= 2;
+};
+
+// Method to display
+Particle.prototype.display = function() {
+  noStroke();
+  fill(this.color[0], this.color[1], this.color[2], this.lifespan);
+  ellipse(this.position.x, this.position.y, 12, 12);
+};
+
+// Is the particle still useful?
+Particle.prototype.isDead = function(){
+  return this.lifespan < 0;
+};
+
+var ParticleSystem = function(position) {
+  this.origin = position.copy();
+  this.particles = [];
+};
+
+ParticleSystem.prototype.addParticle = function(xpos, ypos) {
+  this.particles.push(new Particle(createVector(xpos, ypos)));
+};
+
+ParticleSystem.prototype.run = function() {
+  for (var i = this.particles.length-1; i >= 0; i--) {
+    var p = this.particles[i];
+    p.run();
+    if (p.isDead()) {
+      this.particles.splice(i, 1);
+    }
+  }
+};
