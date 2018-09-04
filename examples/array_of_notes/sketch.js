cd6a73221052aaf99acd341c3e57818a6e5bbac2
JunShern <chanjunshern@gmail.com>
2018-09-04 16:02:31 +0800

    Format code for static song playback

--- examples/array_of_notes/sketch.js
+++ examples/array_of_notes/sketch.js
@@ -1,23 +1,23 @@
 var synth;
 var songStarted = false;
-var song = [ 
+var song = [
   // Note pitch, velocity (between 0-1), start time (s), note duration (s)
-  {pitch:'E4', velocity:1, time:0, duration:1},
-  {pitch:'D4', velocity:1, time:1, duration:1},
-  {pitch:'C4', velocity:1, time:2, duration:1},
-  {pitch:'D4', velocity:1, time:3, duration:1},
-  {pitch:'E4', velocity:1, time:4, duration:1},
-  {pitch:'E4', velocity:1, time:5, duration:1},
-  {pitch:'E4', velocity:1, time:6, duration:1},
+  { pitch: 'E4', velocity: 1, time: 0, duration: 1 },
+  { pitch: 'D4', velocity: 1, time: 1, duration: 1 },
+  { pitch: 'C4', velocity: 1, time: 2, duration: 1 },
+  { pitch: 'D4', velocity: 1, time: 3, duration: 1 },
+  { pitch: 'E4', velocity: 1, time: 4, duration: 1 },
+  { pitch: 'E4', velocity: 1, time: 5, duration: 1 },
+  { pitch: 'E4', velocity: 1, time: 6, duration: 1 },
   // Rest indicated by offset in start time
-  {pitch:'D4', velocity:1, time:8, duration:1},
-  {pitch:'D4', velocity:1, time:9, duration:1},
-  {pitch:'E4', velocity:1, time:10, duration:1},
-  {pitch:'D4', velocity:1, time:11, duration:1},
+  { pitch: 'D4', velocity: 1, time: 8, duration: 1 },
+  { pitch: 'D4', velocity: 1, time: 9, duration: 1 },
+  { pitch: 'E4', velocity: 1, time: 10, duration: 1 },
+  { pitch: 'D4', velocity: 1, time: 11, duration: 1 },
   // Chord indicated by simultaneous note start times
-  {pitch:'C4', velocity:1, time:12, duration:2},
-  {pitch:'E4', velocity:1, time:12, duration:2},
-  {pitch:'G4', velocity:1, time:12, duration:2},
+  { pitch: 'C4', velocity: 1, time: 12, duration: 2 },
+  { pitch: 'E4', velocity: 1, time: 12, duration: 2 },
+  { pitch: 'G4', velocity: 1, time: 12, duration: 2 },
 ];
 
 function setup() {
@@ -28,17 +28,17 @@ function setup() {
 function draw() {
   background(255);
   if (songStarted) {
-    text('song started', width/2, height/2);
+    text('song started', width / 2, height / 2);
   } else {
-	  text('click to play song', width/2, height/2);
+    text('click to play song', width / 2, height / 2);
   }
 }
 
 function touchStarted() {
   if (!songStarted) { // Only play once
-    for (var i=0; i<song.length; i++) {
+    for (var i = 0; i < song.length; i++) {
       note = song[i];
-      synth.play(note.pitch, note.velocity, note.time, note.duration*0.9);
+      synth.play(note.pitch, note.velocity, note.time, note.duration);
     }
     songStarted = true;
   }
