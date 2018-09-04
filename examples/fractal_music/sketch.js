1dee30e1961ad6d042c0491444692916d7c9d256
JunShern <chanjunshern@gmail.com>
2018-09-04 17:00:42 +0800

    Change from +/- to A,B,C symbols, and use textWidth for accurate highlighting

--- examples/fractal_music/sketch.js
+++ examples/fractal_music/sketch.js
@@ -1,30 +1,39 @@
 /**
- *  Example: Fractal Music
- *  Generate a fractal pattern of music using L-Systems. 
- *  We begin with an initial sequence of tokens, and use a programmer-
- *  defined set of rules to generate new tokens. Here, we apply the rules:
- *  + > + -
- *  - > +
- *  to each token in existing sequences to generate new sequences. The 
- *  tokens used here are "+","-" which steer the melody's pitch upward or 
- *  downward. Note durations are selected randomly.
+ * Example: Fractal Music
+ * Generate a fractal pattern of music using L-Systems. 
+ * 
+ * We begin with an initial sequence of tokens, and use a programmer-
+ * defined set of rules to generate new tokens. Here, we apply the rules:
+ * A -> C, F, E
+ * C -> G, E
+ * E -> C, G
+ * F -> A, C
+ * G -> E, F, C
+ * to each token in existing sequences to generate new sequences. The 
+ * tokens used here are plain note names from a fixed octave. Note durations 
+ * are selected randomly from a palette of "nice" durations.
  */
 
 var rules = {
-  "+":["+","-"],
-  "-":["+"]
+  "A":["C","F","E"],
+  "C":["G","E"],
+  "E":["C","G"],
+  "F":["A","C"],
+  "G":["E","F","C"]
 };
 var seqIndex = 0;
 var noteIndex = -1;
-var initSeq = ["+","-","+"];
+var initSeq = ["C"];
 var newTokens = [];
+var sequences = [initSeq, []];
+var maxNumSequences = 8;
+var maxSequenceLength = 30;
+
 var generatingTokenColor;
 var newTokenColor;
-var sequences = [initSeq, []];
 var fontSize = 20;
-var maxNumSequences = 8;
-var maxSequenceLength = 25;
-var pitch = 60;
+var highlightWidth = fontSize;
+var highlightHeight = fontSize;
 
 function setup() {
   createCanvas(720, 400);
@@ -35,13 +44,13 @@ function setup() {
   newTokenColor  = color(240);
 
   synth = new p5.PolySynth();
-  sloop = new p5.SoundLoop(soundLoop, 0.5);
+  sloop = new p5.SoundLoop(soundLoop, 0.7);
 
   playPauseButton = createButton('Play/Pause');
-  playPauseButton.position(width - 80, height - 20);
+  playPauseButton.position(width - 2*playPauseButton.size().width, height - 2*playPauseButton.size().height);
   playPauseButton.mousePressed(togglePlayPause);
   stepButton = createButton("Step");
-  stepButton.position(10, height - 20);
+  stepButton.position(2*stepButton.size().width, height - 2*stepButton.size().height);
   stepButton.mousePressed(stepSoundLoop)
 }
 
@@ -50,19 +59,15 @@ function soundLoop(cycleStartTime) {
   if (noteIndex >= min(sequences[seqIndex].length, maxSequenceLength)) {
     nextSequence();
   }
+  var token = sequences[seqIndex][noteIndex];
 
-  var beatSeconds = 0.5; // Define 1 beat as half a second
-  if (sequences[seqIndex][noteIndex] === "+") {
-    pitch++;
-  } else {
-    pitch--;
-  }
+  var pitch = token + "4";
   var velocity = 0.8;
+  var beatSeconds = 0.5; // Define 1 beat as half a second
   var duration = random([beatSeconds, beatSeconds/2, beatSeconds/2, beatSeconds/4]);
   this.interval = duration;
-  synth.play(midiToFreq(pitch), velocity, cycleStartTime, duration);
+  synth.play(pitch, velocity, cycleStartTime, duration);
 
-  var token = sequences[seqIndex][noteIndex];
   newTokens = rules[token];
   sequences[seqIndex+1] = sequences[seqIndex+1].concat(newTokens);
   // If the sequence overruns maxSequenceLength, truncate it and proceed to next sequence
@@ -91,10 +96,6 @@ function draw() {
     var lineHeight = fontSize + 10;
     text(seq.join(" "), width/2, height*2/3 - lineHeight * (sequences.length - i - 1));
   }
-
-  // Display current pitch
-  fill(100);
-  text("Pitch: " + pitch, width/2, height-20);
 }
 
 function togglePlayPause() {
@@ -115,7 +116,7 @@ function stepSoundLoop() {
 function nextSequence() {
   noteIndex = 0;
   seqIndex++;
-  sequences.push(rules[sequences[seqIndex][0]]); // Add a new sequence with the first tokens generated
+  sequences.push([]); // Add a new empty sequence
   // If the number of sequences overruns maxNumSequences, remove oldest
   if (sequences.length > maxNumSequences) {
     seqIndex--;
@@ -125,13 +126,14 @@ function nextSequence() {
 
 function highlightNote(seqInd, noteInd, highlightColor) {
   if (noteInd < 0) return; // Skip if we haven't started
-  var seqWidth = (fontSize+4) * sequences[seqInd].length;
+  // X position of character
+  var seqWidth = textWidth(sequences[seqInd].join(" "));
   var xOffset = width/2 - seqWidth/2;
   var x = xOffset + seqWidth * noteInd / sequences[seqInd].length;
+  // Y position of character
   var lineHeight = fontSize + 10;
   var y = fontSize + height * 2 / 3 - lineHeight * (sequences.length - seqInd - 0);
-  var highlightWidth = fontSize;
-  var highlightHeight = fontSize;
+  
   strokeWeight(1);
   stroke(150);
   fill(highlightColor);
