d90c3fc7bd97b0996d5d97b5a2615ca50fcca091
Oren Shoham <oren.avraham.shoham@gmail.com>
2019-06-25 03:26:56 -0400

    Replace scriptProcessorNode in p5.SoundRecorder with transpiled AudioWorkletNode; polyfill AudioWorklet

--- webpack.config.js
+++ webpack.config.js
@@ -37,12 +37,9 @@ module.exports = {
         test: /\.js$/,
         exclude: /(node_modules)/,
         use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env']
-          }
+          loader: 'babel-loader'
         }
-      }
+      },
     ]
   },
   optimization: {
