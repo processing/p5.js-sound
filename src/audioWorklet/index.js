e64ba8a121a285b957b8cd9b004053b86cabc237
Jason Sigal <jsigal@spotify.com>
2019-09-04 10:22:36 -0400

    only add audioworklet modules once
    This Promise `return ac.audioWorklet.addModule(objectURL)` was throwing an error if the worklet with that name had already been registered on the page. We run into this issue when running p5 in "instance mode" (i.e. on the documentation examples at p5js.org).

--- src/audioWorklet/index.js
+++ src/audioWorklet/index.js
@@ -6,6 +6,8 @@ const moduleSources = [
 ];
 const ac = p5sound.audiocontext;
 
+let initializedAudioWorklets = false;
+
 function loadAudioWorkletModules() {
   return Promise.all(moduleSources.map(function(moduleSrc) {
     const blob = new Blob([moduleSrc], { type: 'application/javascript' });
@@ -15,13 +17,16 @@ function loadAudioWorkletModules() {
 }
 
 p5.prototype.registerMethod('init', function() {
+  if (initializedAudioWorklets) return;
   // ensure that a preload function exists so that p5 will wait for preloads to finish
   if (!this.preload && !window.preload) {
     this.preload = function() {};
   }
+
   // use p5's preload system to load necessary AudioWorklet modules before setup()
-  this._preloadCount++;
+  this._incrementPreload();
   const onWorkletModulesLoad = function() {
+    initializedAudioWorklets = true;
     this._decrementPreload();
   }.bind(this);
   loadAudioWorkletModules().then(onWorkletModulesLoad);
