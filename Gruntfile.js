f0c70554c9d3d9da8a36acb729531eff27c41911
endurance21 <divyanshuraj5050@gmail.com>
2020-07-08 05:07:14 +0530

    fetching latest p5.js code from npm

--- Gruntfile.js
+++ Gruntfile.js
@@ -27,7 +27,7 @@ module.exports = function(grunt) {
           configFile: './.eslintrc',
           fix: true
         },
-        src: ['src/**/*.js', 'test/tests/**/*.js']
+        src: ['sr/**/*.js','test/tests/*.js'] 
       }
     },
     webpack: {
