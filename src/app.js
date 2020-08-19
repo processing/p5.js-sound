c1302d9b489f2baa9ec0b398ff4a04d37105995f
endurance21 <divyanshuraj5050@gmail.com>
2020-08-19 06:05:53 +0530

    Replacement of es5 functiond to es6 classes feat p5.SoundRecorder

--- src/app.js
+++ src/app.js
@@ -42,21 +42,14 @@ p5.BandPass = BandPass;
 import EQ from './eq';
 p5.EQ = EQ;
 
-
-
 import listener3D from './listener3d';
 p5.listener3D = listener3D;
-
-
 
 import Panner3D from './panner3d';
 p5.Panner3D = Panner3D;
 
-
-
 import Delay from './delay';
 p5.Delay = Delay;
-
 
 import './reverb';
 
@@ -69,12 +62,11 @@ import './soundLoop';
 import Compressor from './compressor';
 p5.Compressor = Compressor;
 
-import './soundRecorder';
-
-
 import peakDetect from './peakDetect';
 p5.peakDetect = peakDetect;
 
+import SoundRecorder from './soundRecorder';
+p5.SoundRecorder = SoundRecorder;
 
 import Distortion from './distortion';
 p5.Distortion = Distortion;
@@ -88,11 +80,8 @@ p5.AudioVoice = AudioVoice;
 import MonoSynth from './monosynth';
 p5.MonoSynth = MonoSynth;
 
-
-
 import OnsetDetect from './onsetDetect';
 p5.OnsetDetect = OnsetDetect;
 
 import PolySynth from './polysynth';
 p5.PolySynth = PolySynth;
-
