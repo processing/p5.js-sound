9669282ba6bc0507cd0c9c0e97c51d46b268981f
Pragya <divyanshuraj5050@gmail.com>
2020-08-20 18:01:37 -0400

    Replacement of es5 functions to es6 classes feat p5.panner

--- src/app.js
+++ src/app.js
@@ -12,7 +12,8 @@ p5.prototype.freqToMidi = freqToMidi;
 
 import './errorHandler';
 import './audioWorklet';
-import './panner';
+import Panner from './panner';
+p5.Panner = Panner;
 import './soundfile';
 
 import Amplitude from './amplitude';
@@ -42,21 +43,14 @@ p5.BandPass = BandPass;
 import EQ from './eq';
 p5.EQ = EQ;
 
-
-
 import listener3D from './listener3d';
 p5.listener3D = listener3D;
-
-
 
 import Panner3D from './panner3d';
 p5.Panner3D = Panner3D;
 
-
-
 import Delay from './delay';
 p5.Delay = Delay;
-
 
 import './reverb';
 
@@ -71,10 +65,8 @@ p5.Compressor = Compressor;
 
 import './soundRecorder';
 
-
 import peakDetect from './peakDetect';
 p5.peakDetect = peakDetect;
-
 
 import Distortion from './distortion';
 p5.Distortion = Distortion;
@@ -88,11 +80,8 @@ p5.AudioVoice = AudioVoice;
 import MonoSynth from './monosynth';
 p5.MonoSynth = MonoSynth;
 
-
-
 import OnsetDetect from './onsetDetect';
 p5.OnsetDetect = OnsetDetect;
 
 import PolySynth from './polysynth';
 p5.PolySynth = PolySynth;
-
