46ac26d08370f6b979383f707da2f303ec8e250f
endurance21 <divyanshuraj5050@gmail.com>
2020-08-21 05:05:06 +0530

    decoupled helpers method from p5 in helper.js and coupled them separately in app.js

--- src/app.js
+++ src/app.js
@@ -7,8 +7,36 @@ p5.prototype.userStartAudio = userStartAudio;
 
 import './master';
 
-import { freqToMidi } from './helpers';
+import {
+  sampleRate,
+  freqToMidi,
+  midiToFreq,
+  noteToFreq,
+  soundFormats,
+  disposeSound,
+  _checkFileFormats,
+  _mathChain,
+  convertToWav,
+  interleave,
+  writeUTFBytes,
+  safeBufferSize,
+} from './helpers';
+p5.prototype.sampleRate = sampleRate;
 p5.prototype.freqToMidi = freqToMidi;
+p5.prototype.midiToFreq = midiToFreq;
+p5.prototype.noteToFreq = noteToFreq;
+p5.prototype.soundFormats = soundFormats;
+p5.prototype.disposeSound = disposeSound;
+p5.prototype._checkFileFormats = _checkFileFormats;
+p5.prototype._mathChain = _mathChain;
+p5.prototype.convertToWav = convertToWav;
+p5.prototype.interleave = interleave;
+p5.prototype.writeUTFBytes = writeUTFBytes;
+p5.prototype.safeBufferSize = safeBufferSize;
+
+// register removeSound to dispose of p5sound SoundFiles, Convolvers,
+// Oscillators etc when sketch ends
+p5.prototype.registerMethod('remove', p5.prototype.disposeSound);
 
 import './errorHandler';
 import './audioWorklet';
@@ -42,21 +70,14 @@ p5.BandPass = BandPass;
 import EQ from './eq';
 p5.EQ = EQ;
 
-
-
 import listener3D from './listener3d';
 p5.listener3D = listener3D;
-
-
 
 import Panner3D from './panner3d';
 p5.Panner3D = Panner3D;
 
-
-
 import Delay from './delay';
 p5.Delay = Delay;
-
 
 import './reverb';
 
@@ -71,10 +92,8 @@ p5.Compressor = Compressor;
 
 import './soundRecorder';
 
-
 import peakDetect from './peakDetect';
 p5.peakDetect = peakDetect;
-
 
 import Distortion from './distortion';
 p5.Distortion = Distortion;
@@ -88,11 +107,8 @@ p5.AudioVoice = AudioVoice;
 import MonoSynth from './monosynth';
 p5.MonoSynth = MonoSynth;
 
-
-
 import OnsetDetect from './onsetDetect';
 p5.OnsetDetect = OnsetDetect;
 
 import PolySynth from './polysynth';
 p5.PolySynth = PolySynth;
-
