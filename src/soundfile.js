119ee07706e9a5efd6f2a34b57e8f494832c11c2
Ondřej Exner <ondra.ex@gmail.com>
2020-09-19 20:08:33 +0200

    fix SoundFile.processPeaks()

--- src/soundfile.js
+++ src/soundfile.js
@@ -1515,6 +1515,7 @@ class SoundFile {
    *  @return {Array}                  Array of timestamped peaks
    */
   processPeaks(callback, _initThreshold, _minThreshold, _minPeaks) {
+    var self = this;
     var bufLen = this.buffer.length;
     var sampleRate = this.buffer.sampleRate;
     var buffer = this.buffer;
