2fd6a9012f0e0ea4bb70a6ca784661674e453fc3
DIVYANSHU RAJ <43696525+endurance21@users.noreply.github.com>
2021-02-04 21:47:19 +0530

    modernized the  TESTING architecture of the codebase { GSOC}  (#541)
    * modernized the  TESTING architecture of the codebase
    
    * added sinon.js support for soundRecorder.js testing and removed use strict declaration
    
    * * src/audioWorklet/index.js - added p5.audioWorkletInitilized  promise to ensure proper loading of AudioWorklet modules for
                                * sounFile  processor
                                * Oscillator processor
                                * Amplitude processor
    
     * test/index.js -  then used dynamic import to import the tests after each of worklet processors have been loaded successfully
    
    * solved loading of audioWorklet processors in testing by use of p5 instance mode
    
    * fixed audioworklet import
    
    Co-authored-by: Divyanshu Raj <endurance21@pop-os.localdomain>

--- src/audioWorklet/index.js
+++ src/audioWorklet/index.js
@@ -6,7 +6,6 @@ const moduleSources = [
 ];
 const ac = p5sound.audiocontext;
 
-let initializedAudioWorklets = false;
 
 function loadAudioWorkletModules() {
   return Promise.all(
@@ -19,7 +18,6 @@ function loadAudioWorkletModules() {
 }
 
 p5.prototype.registerMethod('init', function () {
-  if (initializedAudioWorklets) return;
   // ensure that a preload function exists so that p5 will wait for preloads to finish
   if (!this.preload && !window.preload) {
     this.preload = function () {};
@@ -28,7 +26,6 @@ p5.prototype.registerMethod('init', function () {
   // use p5's preload system to load necessary AudioWorklet modules before setup()
   this._incrementPreload();
   const onWorkletModulesLoad = function () {
-    initializedAudioWorklets = true;
     this._decrementPreload();
   }.bind(this);
   loadAudioWorkletModules().then(onWorkletModulesLoad);
--- test/tests/p5.Oscillator.js
+++ test/tests/p5.Oscillator.js
@@ -1,89 +1,85 @@
-'use strict';
+var expect = chai.expect;
 
-define(['chai'], function (chai) {
-  var expect = chai.expect;
+describe('p5.Oscillator', function () {
+  this.timeout(1000);
 
-  describe('p5.Oscillator', function () {
-    this.timeout(1000);
+  var osc = new p5.Oscillator();
+  var amp = new p5.Amplitude();
 
-    var osc = new p5.Oscillator();
-    var amp = new p5.Amplitude();
+  after(function () {
+    osc.dispose();
+  });
 
-    after(function () {
-      osc.dispose();
-    });
+  it('can be created and disposed', function () {
+    var o = new p5.Oscillator();
+    o.dispose();
+  });
 
-    it('can be created and disposed', function () {
-      var o = new p5.Oscillator();
-      o.dispose();
-    });
+  it('starts and stops', function (done) {
+    expect(osc.started).to.equal(false);
+    osc.start();
+    expect(osc.started).to.equal(true);
+    setTimeout(function () {
+      osc.stop();
+      done();
+    }, 100);
+  });
 
-    it('starts and stops', function (done) {
+  it('can be scheduled to stop', function (done) {
+    osc.stop();
+    expect(osc.started).to.equal(false);
+    osc.start();
+    expect(osc.started).to.equal(true);
+    osc.stop(0.05);
+    setTimeout(function () {
       expect(osc.started).to.equal(false);
-      osc.start();
-      expect(osc.started).to.equal(true);
-      setTimeout(function () {
-        osc.stop();
-        done();
-      }, 100);
-    });
+      done();
+    }, 55);
+  });
 
-    it('can be scheduled to stop', function (done) {
+  it('wont start again before stopping', function (done) {
+    expect(osc.started).to.equal(false);
+    setTimeout(function () {
+      osc.amp(1);
+      osc.start();
       osc.stop();
       expect(osc.started).to.equal(false);
       osc.start();
-      expect(osc.started).to.equal(true);
-      osc.stop(0.05);
-      setTimeout(function () {
-        expect(osc.started).to.equal(false);
-        done();
-      }, 55);
-    });
-
-    it('wont start again before stopping', function (done) {
-      expect(osc.started).to.equal(false);
-      setTimeout(function () {
-        osc.amp(1);
-        osc.start();
-        osc.stop();
-        expect(osc.started).to.equal(false);
-        osc.start();
-        osc.start();
-        amp.setInput(osc);
-        amp.getLevel();
-        setTimeout(function () {
-          expect(osc.started).to.equal(true);
-          expect(amp.volMax).not.equal(0.0);
-          osc.stop();
-          expect(osc.started).to.equal(false);
-          done();
-        }, 5);
-      }, 1);
-    });
-
-    // it('can set the frequency', function(done){
-    //   var currentFreq = osc.getFreq();
-    //   osc.freq(220, 0, 0.15);
-    //   osc.start();
-    //   expect(osc.getFreq()).to.equal(currentFreq);
-    //   setTimeout(function(){
-    //     expect(osc.getFreq()).to.equal(220);
-    //     osc.stop();
-    //     done();
-    //   }, 15);
-    // });
-
-    it('can start in the future', function (done) {
-      expect(osc.started).to.equal(false);
-      osc.start(0.05);
-      // expect( amp.getLevel() ).to.be.closeTo(0.0, 0.5);
+      osc.start();
+      amp.setInput(osc);
+      amp.getLevel();
       setTimeout(function () {
         expect(osc.started).to.equal(true);
-        // expect( amp.getLevel ).to.not.equal(0.0);
+        expect(amp.volMax).not.equal(0.0);
         osc.stop();
         expect(osc.started).to.equal(false);
         done();
-      }, 55);
-    });
+      }, 5);
+    }, 1);
+  });
+
+  // it('can set the frequency', function(done){
+  //   var currentFreq = osc.getFreq();
+  //   osc.freq(220, 0, 0.15);
+  //   osc.start();
+  //   expect(osc.getFreq()).to.equal(currentFreq);
+  //   setTimeout(function(){
+  //     expect(osc.getFreq()).to.equal(220);
+  //     osc.stop();
+  //     done();
+  //   }, 15);
+  // });
+
+  it('can start in the future', function (done) {
+    expect(osc.started).to.equal(false);
+    osc.start(0.05);
+    // expect( amp.getLevel() ).to.be.closeTo(0.0, 0.5);
+    setTimeout(function () {
+      expect(osc.started).to.equal(true);
+      // expect( amp.getLevel ).to.not.equal(0.0);
+      osc.stop();
+      expect(osc.started).to.equal(false);
+      done();
+    }, 55);
   });
 });
