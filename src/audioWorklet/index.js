0091905265a60ad8cac70f751cc4aafe696544c6
Divyanshu Raj <divyanshuraj5050@gmail.com>
2021-03-17 14:20:39 +0530

    fixed some mistakes

--- src/audioWorklet/index.js
+++ src/audioWorklet/index.js
@@ -5,6 +5,7 @@ const moduleSources = [
   require('raw-loader!./amplitudeProcessor').default,
 ];
 const ac = p5sound.audiocontext;
+let initializedAudioWorklets = false;
 
 function loadAudioWorkletModules() {
   return Promise.all(
@@ -26,6 +27,7 @@ p5.prototype.registerMethod('init', function () {
   // use p5's preload system to load necessary AudioWorklet modules before setup()
   this._incrementPreload();
   const onWorkletModulesLoad = function () {
+    initializedAudioWorklets = true;
     this._decrementPreload();
   }.bind(this);
   loadAudioWorkletModules().then(onWorkletModulesLoad);
