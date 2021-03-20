e477fa282604f6291e636b4915fd78b43fb2b38a
DIVYANSHU RAJ <43696525+endurance21@users.noreply.github.com>
2021-03-20 12:24:06 +0530

    fixed repeated initilisation of audioWorklet (#593)
    * fixed repeated initilisation of audioWorklet
    
    * fixed some mistakes

--- src/audioWorklet/index.js
+++ src/audioWorklet/index.js
@@ -5,6 +5,7 @@ const moduleSources = [
   require('raw-loader!./amplitudeProcessor').default,
 ];
 const ac = p5sound.audiocontext;
+let initializedAudioWorklets = false;
 
 function loadAudioWorkletModules() {
   return Promise.all(
@@ -17,6 +18,7 @@ function loadAudioWorkletModules() {
 }
 
 p5.prototype.registerMethod('init', function () {
+  if (initializedAudioWorklets) return;
   // ensure that a preload function exists so that p5 will wait for preloads to finish
   if (!this.preload && !window.preload) {
     this.preload = function () {};
@@ -25,6 +27,7 @@ p5.prototype.registerMethod('init', function () {
   // use p5's preload system to load necessary AudioWorklet modules before setup()
   this._incrementPreload();
   const onWorkletModulesLoad = function () {
+    initializedAudioWorklets = true;
     this._decrementPreload();
   }.bind(this);
   loadAudioWorkletModules().then(onWorkletModulesLoad);
