09996ecba7a9fb5b049bc826bd4d8622e9bde02e
rohit <isinghrohit0@gmail.com>
2021-03-31 01:23:03 +0530

    Fixing after review and adding test for the soundFormats

--- test/tests/p5.Helpers.js
+++ test/tests/p5.Helpers.js
@@ -1,16 +1,35 @@
-var expect = chai.expect;
+const expect = chai.expect;
 
 describe('Testing helpers function', function () {
   it('p5.prototype.freqToMidi helper function', function () {
-    let Midi = p5.prototype.freqToMidi(880);
-    expect(Midi).to.equal(81);
+    const midi = p5.prototype.freqToMidi(880);
+    expect(midi).to.equal(81);
   });
+
   it('p5.prototype.midiToFreq  helper function', function () {
-    let Freq = p5.prototype.midiToFreq(720);
-    expect(Freq).to.equal(9426054387186970000);
+    const freq = p5.prototype.midiToFreq(720);
+    expect(freq).to.equal(9426054387186970000);
   });
+
   it('p5.prototype.noteToFreq  helper function', function () {
-    let Freq = p5.prototype.noteToFreq('C4');
-    expect(Freq).to.equal(261.6255653005986);
+    const freq = p5.prototype.noteToFreq('C4');
+    expect(freq).to.equal(261.6255653005986);
+  });
+
+  it('p5.prototype.soundFormats  helper function', function () {
+    // setting file format so that if we don't provide extension
+    // our file will be loaded because _checkFileFormats add it for us.
+
+    p5.prototype.soundFormats('mp3');
+    const file = p5.prototype._checkFileFormats('a');
+    expect(file).to.be.equal('a.mp3');
+
+    // if we don't provide a valid sound format then soundFormats wil throw
+    //error
+    try {
+      p5.prototype.soundFormats('ext');
+    } catch (err) {
+      expect(err).to.be.equal('ext is not a valid sound format!');
+    }
   });
 });
