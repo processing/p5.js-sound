bbe76c0f3d7564069872c22d029bd45b00c2e78d
Divyanshu Raj <divyanshuraj5050@gmail.com>
2021-04-28 13:13:33 +0530

    added unit tests for p5.fft

--- test/tests/p5.FFT.js
+++ test/tests/p5.FFT.js
@@ -55,4 +55,55 @@ describe('p5.FFT', function () {
     expect(fft.smoothing).to.equal(0.8);
     expect(fft.smooth()).to.equal(0.8);
   });
+
+  describe('can set input correctly', function () {
+    let fft;
+
+    beforeEach(function () {
+      fft = new p5.FFT();
+    });
+
+    afterEach(function () {
+      fft.dispose();
+    });
+
+    let soundFile, oscillator;
+    it('can set Oscillator as input', function (done) {
+      this.timeout(1000);
+      oscillator = new p5.Oscillator();
+      fft.setInput(oscillator);
+      oscillator.start();
+
+      setTimeout(() => {
+        let spectrum = fft.analyze();
+        const someFrequencyIsNotZero = spectrum.some(
+          (frequency) => frequency !== 0
+        );
+        expect(someFrequencyIsNotZero).to.equal(true);
+        oscillator.dispose();
+        done();
+      }, 500);
+    });
+    it('can set soundFile  as input', function () {
+      soundFile = p5.prototype.loadSound('./testAudio/drum.mp3', () => {
+        fft.setInput(soundFile);
+        soundFile.play();
+        let spectrum = fft.analyze();
+        const someFrequencyIsNotZero = spectrum.some(
+          (frequency) => frequency !== 0
+        );
+        expect(someFrequencyIsNotZero).to.equal(true);
+        done();
+      });
+    });
+    it('can handle any Unknown sources', function () {
+      let UnknownSource = { UnknownSource: 'this is a unknown object' };
+      fft.setInput(UnknownSource);
+      let spectrum = fft.analyze();
+      const someFrequencyIsNotZero = spectrum.some(
+        (frequency) => frequency !== 0
+      );
+      expect(someFrequencyIsNotZero).to.equal(false);
+    });
+  });
 });
