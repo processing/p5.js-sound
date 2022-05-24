b839fbd1276842bf982ed4cec3e052ccd4ce02ca
KevinGrajeda <yokevin20@gmail.com>
2022-05-24 15:37:17 -0500

    fix and add documentation for distortion

--- test/tests/p5.Distortion.js
+++ test/tests/p5.Distortion.js
@@ -10,7 +10,7 @@ describe('p5.Distortion', function () {
     expect(distortion.waveShaperNode).to.have.property('context').to.not.be
       .null;
     //default case
-    expect(distortion.amount).to.equal(500);
+    expect(distortion.amount).to.equal(0.25);
     expect(distortion.waveShaperNode.oversample).to.equal('2x');
 
     distortion.dispose();
@@ -19,7 +19,7 @@ describe('p5.Distortion', function () {
 
   it('can be created with only amount as a parameter', function () {
     let distortion = new p5.Distortion(0.3);
-    expect(distortion.amount).to.equal(600);
+    expect(distortion.amount).to.equal(0.3);
     expect(distortion.waveShaperNode.oversample).to.equal('2x');
 
     //non-numerical value
@@ -27,7 +27,7 @@ describe('p5.Distortion', function () {
   });
   it('can be created with only oversample as a parameter', function () {
     let distortion = new p5.Distortion(undefined, '4x');
-    expect(distortion.amount).to.equal(500);
+    expect(distortion.amount).to.equal(0.25);
     expect(distortion.waveShaperNode.oversample).to.equal('4x');
 
     //non-numerical value
@@ -35,7 +35,7 @@ describe('p5.Distortion', function () {
   });
   it('can be created with both parameters', function () {
     let distortion = new p5.Distortion(0.8, '4x');
-    expect(distortion.amount).to.equal(1600);
+    expect(distortion.amount).to.equal(0.8);
     expect(distortion.waveShaperNode.oversample).to.equal('4x');
   });
 
@@ -48,27 +48,27 @@ describe('p5.Distortion', function () {
 
       //both params
       distortion.process(osc, 0.62, '4x');
-      expect(distortion.amount).to.equal(1240);
+      expect(distortion.amount).to.equal(0.62);
       expect(distortion.waveShaperNode.oversample).to.equal('4x');
     });
     it('can set amount and oversample', function () {
       let distortion = new p5.Distortion();
       //only one param
       distortion.set(0.98);
-      expect(distortion.amount).to.equal(1960);
+      expect(distortion.amount).to.equal(0.98);
       expect(distortion.waveShaperNode.oversample).to.equal('2x');
       distortion.set(undefined, '4x');
-      expect(distortion.amount).to.equal(1960);
+      expect(distortion.amount).to.equal(0.98);
       expect(distortion.waveShaperNode.oversample).to.equal('4x');
 
       //both params
       distortion.set(0.14, '2x');
-      expect(distortion.amount).to.equal(280);
+      expect(distortion.amount).to.equal(0.14);
       expect(distortion.waveShaperNode.oversample).to.equal('2x');
     });
     it('can get amount', function () {
       let distortion = new p5.Distortion(0.43, '2x');
-      expect(distortion.getAmount()).to.equal(860);
+      expect(distortion.getAmount()).to.equal(0.43);
     });
     it('can get over sample', function () {
       let distortion = new p5.Distortion(0.43, '4x');
