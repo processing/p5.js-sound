102556559b9e31189dbf1e616d2185c2efe16dd3
Nitesh639 <nkush9598@gmail.com>
2022-11-26 12:50:28 +0530

    Updated aplitude.disconnect() method documentation

--- src/amplitude.js
+++ src/amplitude.js
@@ -190,6 +190,51 @@ class Amplitude {
     // }
   }
 
+  /**
+   *  Disconnects the output of this p5.Amplitude object.
+   *
+   *  @method disconnect
+   *  @for p5.Amplitude
+   *  @example
+   *  <div><code>
+   *  let sound, amplitude, button;
+   *  function preload(){
+   *    sound = loadSound('assets/beat.mp3');
+   *  }
+   *
+   *  function setup() {
+   *    let cnv = createCanvas(100, 100);
+   *    cnv.mouseClicked(togglePlay);
+   *    button = createButton();
+   *    button.mouseClicked(amp_disconnect);
+   *    button.html('tap to disconnect amplitude');
+   *    button.position(0,100);
+   *    amplitude = new p5.Amplitude();
+   *    sound.loop();
+   *  }
+   *
+   *  function draw() {
+   *    background(220);
+   *    text('tap to play', 20, 20);
+   *    let level = amplitude.getLevel();
+   *    let size = map(level, 0, 1, 0, 200);
+   *    ellipse(width/2, height/2, size, size);
+   *  }
+   *
+   *  function togglePlay() {
+   *    if (sound.isPlaying()){
+   *      sound.pause();
+   *    }
+   *    else{
+   *      sound.play();
+   *        }
+   *    }
+   *  function amp_disconnect(){
+   *     amplitude.disconnect();
+   *     button.html('amplitude disconnect now');
+   *     }
+   *  </code></div>
+   */
   disconnect() {
     if (this.output) {
       this.output.disconnect();
