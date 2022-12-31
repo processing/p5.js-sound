5310950030e8fb8780f9a42c1ce600aed0a34576
Abhijay Jain <abhijay007j@gmail.com>
2022-12-31 19:41:35 +0530

    Fixed conficts in Env.js

--- src/deprecations/Env.js
+++ src/deprecations/Env.js
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 import Envelope from '../envelope';
 
 class Env extends Envelope {
@@ -12,18 +11,3 @@ class Env extends Envelope {
 }
 
 export default Env;
-=======
-import Envelope from '../envelope';
-
-class Env extends Envelope {
-  constructor(t1, l1, t2, l2, t3, l3) {
-    console.warn(
-      'WARNING: p5.Env is now deprecated and may be removed in future versions. ' +
-        'Please use the new p5.Envelope instead.'
-    );
-    super(t1, l1, t2, l2, t3, l3);
-  }
-}
-
-export default Env;
->>>>>>> Stashed changes
